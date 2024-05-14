13da0e7c0a8ed3c6939a3f3c86c9acf020778982
j.ghane <j.ghane@meedc.net>
2024-05-14 14:17:54 +0330

    done

--- app/src/main/java/ir/vtj/cameraimagenopermission/MainActivity.kt
+++ app/src/main/java/ir/vtj/cameraimagenopermission/MainActivity.kt
@@ -1,10 +1,27 @@
 package ir.vtj.cameraimagenopermission
 
+import android.Manifest
+import android.R.attr.bitmap
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.MediaStore
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +34,59 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        val btnTake = findViewById<MaterialButton>(R.id.btnTake)
+        btnTake.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 1234)
+            } else {
+                takeImage()
+            }
+        }
 
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            takeImage()
+        }
+    }
+
+    private fun takeImage() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        resultLauncher.launch(cameraIntent)
+    }
+
+
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // There are no request codes
+            val imageUri = result?.data!!.data
+            val bitmap = result.data?.extras?.get("data") as Bitmap
+            val imgPhoto = findViewById<ImageView>(R.id.imgPhoto)
+            imgPhoto.setImageBitmap(bitmap)
+            val fileName = copyImageIntoCacheAndReturnFileName(bitmap)
+            val tvPath = findViewById<TextView>(R.id.tvPath)
+            tvPath.text = "File:" + fileName
+        }
+    }
+
+    fun copyImageIntoCacheAndReturnFileName(bmp: Bitmap): String {
+        val fileName = File(externalCacheDir!!.absolutePath, "tmp.png")
+        try {
+            FileOutputStream(fileName.absolutePath).use { out ->
+                bmp.compress(Bitmap.CompressFormat.PNG, 100, out) // bmp is your Bitmap instance
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return fileName.absolutePath
+    }
+
+
+    fun getByteOfFile(bitmap: Bitmap): ByteArray {
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos)
+        return baos.toByteArray()
     }
 }
